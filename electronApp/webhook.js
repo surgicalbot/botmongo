abbf47af62df0d75ab7ca1a8129ab5ad313a45ea
surgicalbot <katyuv124@gmail.com>
2017-11-05 04:59:40 -0800

    webhok

--- electronApp/webhook.js
+++ electronApp/webhook.js
@@ -2,7 +2,8 @@ var express = require('express'),
   app = express(),
   http = require('http'),
   httpServer = http.Server(app);
-
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
@@ -12,7 +13,15 @@ app.get('/', function(req, res) {
 app.get('/ricohwebsite', function(req, res) {
   res.sendfile(__dirname + '/myricoh.html');
 });
-
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+ });
 app.get('/chat', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });

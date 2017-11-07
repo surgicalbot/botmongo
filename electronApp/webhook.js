09ed30d0888fb2b186bd3e1f45355135ca3ea9c5
38238-hexaware <38238-hexaware@gmail.com>
2017-11-07 18:36:08 +0530

    webhook

--- electronApp/webhook.js
+++ electronApp/webhook.js
@@ -75,7 +75,7 @@ app.post('/', function (req, res) {
   let action = req.body.result.action; // https://dialogflow.com/docs/actions-and-parameters
 
   // Parameters are any entites that Dialogflow has extracted from the request.
-  const parameters = req.body.result.parameters||result.contexts[0].parameters; // https://dialogflow.com/docs/actions-and-parameters
+  const parameters = req.body.result.parameters||req.body.result.contexts[0].parameters; // https://dialogflow.com/docs/actions-and-parameters
   const hospittyp = parameters.hospital_type;
   const surgicaltyp = parameters.surgical_type;
   const treatmentyp = parameters.treatment_type;

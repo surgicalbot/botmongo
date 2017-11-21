687869a8eb0fffcf5eaf5385b0e6fc1e3f0e823d
srini23121987 <ursfriendly.sri23@gmail.com>
2017-11-21 21:21:45 +0530

    new commit

--- electronApp/webhook.js
+++ electronApp/webhook.js
@@ -75,7 +75,7 @@ app.post('/', function (req, res) {
   let action = req.body.result.action; // https://dialogflow.com/docs/actions-and-parameters
 
   // Parameters are any entites that Dialogflow has extracted from the request.
-  const parameters = req.body.result.contexts[0].parameters || req.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
+  const parameters = req.body.result.parameters; // https://dialogflow.com/docs/actions-and-parameters
   if (action == "input.treatment") {
     const treatmentyp = parameters.treatment_type != '' ? parameters.treatment_type : "";
     mongodb.MongoClient.connect("mongodb://admin:admin123@ds149335.mlab.com:49335/hospital", function (err, database) {

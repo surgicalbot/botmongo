993b88c1eeb99c389e566b3151b173cbc52bc1b2
srini23121987 <ursfriendly.sri23@gmail.com>
2017-12-04 23:21:42 +0530

    new commit

--- electronApp/webhook.js
+++ electronApp/webhook.js
@@ -114,6 +114,59 @@ app.post('/', function (req, res) {
       db.close();
     });
   }
+  if(action=="input.statistics"){
+    var hospitalarray = [];
+    const hospitaltype = parameters.hospital_type != '' ? parameters.hospital_type : "";
+    mongodb.MongoClient.connect("mongodb://admin:admin123@ds149335.mlab.com:49335/hospital", function (err, database) {
+      var db = database;
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
+      filterarray = [
+        { $or: [{ "HOSPITAL": hospitaltype.toLowerCase() }, { "HOSPITAL": hospitaltype.toUpperCase() }, { "HOSPITAL": capitalizeFirstLetter(hospitaltype) }, { "HOSPITAL": toTitleCase(hospitaltype) }] }
+      ]
+      db.collection("surgery").find({
+        $and: filterarray
+      }).toArray(function (err, result) {
+        var hospitalarray = [];
+        for (var keys in result) {
+          console.log(result[keys]["HOSPITAL"]);
+          if (hospitalarray.indexOf(result[keys]["HOSPITAL"]) <0) {
+            hospitalarray.push(result[keys]["HOSPITAL"]);
+          }
+        }
+        var finallarray=[];
+        for (var treatsurgiment in surgicalarray) {
+          var html = {};
+          html["title"]=surgicalarray[treatsurgiment];
+          html["payload"]=surgicalarray[treatsurgiment];
+          html["content_type"]="text";
+          finallarray.push(html);
+        }
+        if (html) {
+          res.json({
+            speech:"",
+            displayText: "",
+            source: 'agent',
+            "messages": [
+                {
+                  "type": 4,
+                  "platform": "facebook",
+                  "payload": {
+                    "facebook": {
+                      "text": "Please Choose Statistics?",
+                      "quick_replies": finallarray
+                    }
+                  }
+                }
+              ]
+            })
+        }
+      });
+      db.close();
+    });
+  }
   if(action=="input.hospital"){
 
     var surgicalarray = [];
